35934658316a4c1bc3d5ed45e51fa1711c9a44b7
mehmeturmac <mehmeturmac@gmail.com>
2022-06-15 22:58:10 +0300

    courses page added and courses are listed

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -5,5 +5,6 @@ const courseController = require("../controllers/courseController");
 const router = express.Router();
 
 router.route("/").post(courseController.createCourse);
+router.route("/").get(courseController.getAllCourse);
 
 module.exports = router;
