a429cead9ddbfa7e290bba5026f971b9ddb80b1f
mehmeturmac <mehmeturmac@gmail.com>
2022-06-18 08:05:38 +0300

    user roles added

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
-
+const roleMiddleware = require("../middlewares/roleMiddleware");
 const courseController = require("../controllers/courseController");
 
 const router = express.Router();
 
-router.route("/").post(courseController.createCourse);
+router
+  .route("/")
+  .post(roleMiddleware(["admin", "teacher"]), courseController.createCourse);
 router.route("/").get(courseController.getAllCourses);
 router.route("/:slug").get(courseController.getCourse);
 
